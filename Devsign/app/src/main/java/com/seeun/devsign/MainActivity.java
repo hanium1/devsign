e8934305350c834bffb1b02e41b383b5ef7529d9
1eeeun <se_0215@naver.com>
2017-08-20 05:17:35 +0900

    a

---
+++ Devsign/app/src/main/java/com/seeun/devsign/MainActivity.java
@@ -0,0 +1,45 @@
+package com.seeun.devsign;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    transaction.replace(R.id.content, new HomeFragment()).commit();
+                    return true;
+                case R.id.navigation_history:
+                    transaction.replace(R.id.content, new HistoryFragment()).commit();
+                    return true;
+                case R.id.navigation_information:
+                    transaction.replace(R.id.content, new InformationFragment()).commit();
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
